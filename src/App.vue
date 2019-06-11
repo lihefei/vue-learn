0402f78fd7fd7521b9547d1888c55ca1f475345d
lihefei <121131636@qq.com>
2019-06-11 21:45:15 +0800

    组件注册 Prop 自定义事件 插槽 动态组件 & 异步组件 处理边界情况

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
-  <div id="app">
-    <router-view />
-  </div>
+    <div id="app">
+        <router-view />
+    </div>
 </template>
 
 <style lang="scss">
 html,
 body,
 #app {
-  height: 100%;
+    height: 100%;
 }
 </style>
